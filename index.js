8a1d5d2b4a4db81127fecb13aacd22bca3b34a95
unuefeejovwoke <vuwill7114@gmail.com>
2023-05-28 20:09:02 -0700

    counter, saved previous counts and clear counter

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let coundId = document.getElementById('counter-id')
 let count = 0
 
 function increment() {
-    count = count +1 
+    count += 1
     coundId.innerText = count
 }
 
 // Function to save the counter and display the last stored count
 function saveCounter() {
-    const lastCount = count;
-    document.getElementById("lastCount").textContent = lastCount;
+    let lastCount = count + " - ";
+    document.getElementById("lastCount").textContent += lastCount;
     count = 0; // Reset the counter to 0
     document.getElementById("counter-id").textContent = count;
   }
@@ -20,3 +20,13 @@ function saveCounter() {
     count = 0;
     document.getElementById("counter-id").textContent = count;
   }
+
+
+  let welcomeId = document.getElementById("welcome-id")
+
+  fullName = "Unuefe Ejovwoke"
+  greeting = "Welcome back"
+
+  welcomeId.innerText = greeting + "  " + fullName
+
+  welcomeId += "👋 "
