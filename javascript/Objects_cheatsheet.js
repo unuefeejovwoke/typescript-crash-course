63727be850d31938615ac7c136e00bc64f19f3ce
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-23 10:25:03 -0700

    second object task

--- javascript/Objects_cheatsheet.js
+++ javascript/Objects_cheatsheet.js
@@ -23,3 +23,16 @@ const product = {
     price : 1.99,
     flavors : ["grape", "apple", "cherry"]
 }
+
+//second assignment
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+var fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
