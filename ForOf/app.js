d538845763fb2b354d27de347dc7bf52ce74c4e8
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-27 21:08:17 -0700

    for of intro

--- ForOf/app.js
+++ ForOf/app.js
@@ -33,3 +33,10 @@ for (let subreddit of subreddits) {
 //     console.log(char)
 // }
 
+//  first for of assignment
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let num of numbers){
+    console.log(num*num)
+}
