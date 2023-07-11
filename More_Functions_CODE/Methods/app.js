788023b803c23ab4ad75f67225663440fb0897cc
unuefeejovwoke <vuwill7114@gmail.com>
2023-07-11 11:44:06 -0700

    this method assigment

--- More_Functions_CODE/Methods/app.js
+++ More_Functions_CODE/Methods/app.js
@@ -21,6 +21,14 @@ const cat = {
 const meow2 = cat.meow;
 
 // methods assignment
+// Define an object called square, which will hold methods that have to do with the geometry of squares. It should contain two methods, area and perimeter 
+
+// area should accept the length of a side (all sides are the same in a square) and then return the side squared. 
+
+// perimeter should accept the length of a side and return that side multiplied by 4.
+
+// square.area(10) //100
+// square.perimeter(10) //40
 const square = {
     area(side){
         return side * side
@@ -32,3 +40,26 @@ const square = {
 
 square.area(10)
 square.perimeter(10)
+
+// Define an object called hen.  It should have three properties:
+
+// name should be set to 'Helen'
+
+// eggCount should be set to 0
+
+// layAnEgg should be a method which increments the value of eggCount by 1 and returns the string "EGG".  You'll need to use this.
+
+// hen.name // "Helen"
+// hen.eggCount // 0
+// hen.layAnEgg() // "EGG"
+// hen.layAnEgg() // "EGG"
+// hen.eggCount // 2
+
+const hen = {
+    name: "Helen",
+    eggCount: 0,
+    layAnEgg(eggCount) {
+        this.eggCount++;
+        return "EGG";
+    }
+};
