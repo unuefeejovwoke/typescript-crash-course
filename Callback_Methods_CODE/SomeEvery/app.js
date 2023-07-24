c875bab30d245d11a1c39033454cb01d14eaabd7
unuefeejovwoke <vuwill7114@gmail.com>
2023-07-23 22:02:15 -0700

    some every function assignment

--- Callback_Methods_CODE/SomeEvery/app.js
+++ Callback_Methods_CODE/SomeEvery/app.js
@@ -52,3 +52,12 @@ const movies = [
 
 
 movies.some(movie => movie.year > 2015)
+
+//assignment
+function allEvens(numbers) {
+    return numbers.every(num => num % 2 === 0);
+  }
+
+console.log(allEvens([2, 4, 6, 8])); // Output: true
+console.log(allEvens([1, 4, 6, 8])); // Output: false
+console.log(allEvens([1, 2, 3]));    // Output: false
