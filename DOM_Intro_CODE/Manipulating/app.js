8fa5a66efc9da73963116fe1b780db03795dc17b
unuefeejovwoke <vuwill7114@gmail.com>
2023-07-30 21:48:06 -0700

    getattribute assignment

--- DOM_Intro_CODE/Manipulating/app.js
+++ DOM_Intro_CODE/Manipulating/app.js
@@ -14,5 +14,15 @@ for (let link of allLinks) {
 
 //queryselector assigment
 // YOUR CODE GOES IN HERE:
-const span = document.querySelector('span');
+const span = document.querySelector('b');
 span.innerHTML = "Disgusting";
+
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+const imageElement = document.querySelector('img');
+
+        // Change the source attribute using setAttribute()
+imageElement.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+
+        // Change the alt text attribute using setAttribute()
+imageElement.setAttribute('alt', 'chicken');
