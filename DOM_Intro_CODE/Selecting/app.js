8d823c8b6fb31bbb2a37410bfc3a1bfcdb83095d
unuefeejovwoke <vuwill7114@gmail.com>
2023-07-30 21:17:31 -0700

    queryselector

--- DOM_Intro_CODE/Selecting/app.js
+++ DOM_Intro_CODE/Selecting/app.js
@@ -1,9 +1,9 @@
-const allImages = document.getElementsByTagName('img');
+// const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
-
+// for (let img of allImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// }
+// replace images
 
 const squareImages = document.getElementsByClassName('square');
 
@@ -21,3 +21,8 @@ for (let link of links) {
 // Write your code in here:
 const image = document.getElementById('unicorn');
 const heading = document.getElementById('mainheading')
+
+//get element by queryselector assignment
+// Your code goes in here!
+const doneTodos = document.querySelectorAll('.done');
+const checkbox = document.querySelector('input[type=checkbox]')
