df9c94d75c99929b199244ec481ee1885c20620c
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-08 20:30:57 -0700

    js changing style assignment

---
+++ DOM_Intro_CODE/Manipulating/assignment.js
@@ -0,0 +1,6 @@
+const text = document.getElementById("container");
+text.style.textAlign = 'center';
+
+const img = document.querySelector("img");
+img.style.width = "150px";
+img.style.borderRadius = "50%";
