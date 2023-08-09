ad542b9bdd12bff8a5bb53e1f77eadd63c1acab4
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-08 21:43:54 -0700

    classlist assignment

--- DOM_Intro_CODE/Manipulating/assignment.js
+++ DOM_Intro_CODE/Manipulating/assignment.js
@@ -4,3 +4,23 @@ text.style.textAlign = 'center';
 const img = document.querySelector("img");
 img.style.width = "150px";
 img.style.borderRadius = "50%";
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+const spans = document.querySelectorAll('h1 span');
+
+// Iterate over spans and assign colors
+spans.forEach((span, index) => {
+  span.style.color = colors[index]; // Assign color based on index
+});
+
+// WRITE YOUR CODE IN HERE:
+
+// Get all <li> elements
+const listItems = document.querySelectorAll('li');
+
+// Iterate over list items and toggle the 'highlight' class
+listItems.forEach(li => {
+  li.classList.toggle('highlight');
+});
